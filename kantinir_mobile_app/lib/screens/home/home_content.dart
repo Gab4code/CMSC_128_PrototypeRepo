a12e9e6ba53d9e041f9e712051a7df92f042bb3f
Gab <gfnatividad@up.edu.ph>
2024-01-08 19:56:11 +0800

    profile picture done

--- kantinir_mobile_app/lib/screens/home/home_content.dart
+++ kantinir_mobile_app/lib/screens/home/home_content.dart
@@ -85,7 +85,7 @@ class _Home_contentPageState extends State<Home_contentPage> {
             children: [
               
                 Image.network(
-                  'https://i.pinimg.com/originals/48/c9/52/48c9522aaa31a27582216bec737e92ce.gif',
+                  'https://media.tenor.com/RcX3hUY425kAAAAi/toothless-dragon-toothless.gif',
                   height: 80,
                   width: 80,
                 ),
