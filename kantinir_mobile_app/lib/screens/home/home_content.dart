297f59ac4d4d652575c7b49a5fa16095b2a5548a
Gab <gfnatividad@up.edu.ph>
2024-01-09 02:48:36 +0800

    added partial homepage

--- kantinir_mobile_app/lib/screens/home/home_content.dart
+++ kantinir_mobile_app/lib/screens/home/home_content.dart
@@ -11,9 +11,10 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:kantinir_mobile_app/services/my_list_tile.dart';
 import '../food_page/foodPage.dart';
 import '../housing_page/housingPage.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Home_contentPage extends StatefulWidget {
-  const Home_contentPage({super.key});
+  const Home_contentPage({Key? key}) : super(key: key);
 
   @override
   State<Home_contentPage> createState() => _Home_contentPageState();
@@ -22,113 +23,208 @@ class Home_contentPage extends StatefulWidget {
 class _Home_contentPageState extends State<Home_contentPage> {
   final currentUser = FirebaseAuth.instance.currentUser!;
   bool _showImage = false;
+  Widget currentPage = Container();
+  int currentIndex = 0;
+
+  
   
 
 
   @override
+  void initState() {
+    super.initState();
+    _showWelcomeDialog();
+  }
+
+  void _showWelcomeDialog() {
+    Future.delayed(Duration.zero, () {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Welcome!"),
+            content: StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("Users")
+                  .doc(currentUser.email)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final userData =
+                      snapshot.data!.data() as Map<String, dynamic>;
+                  return Text(
+                    "Welcome, ${userData["username"]}!",
+                    style: TextStyle(fontSize: 18),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error ${snapshot.error}');
+                }
+                return CircularProgressIndicator();
+              },
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Close'),
+              ),
+            ],
+          );
+        },
+      );
+    });
+  }
+
+  
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StreamBuilder<DocumentSnapshot>(
-          stream: FirebaseFirestore.instance
-          .collection("Users")
-          .doc(currentUser.email)
-          .snapshots(),
-
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final userData = snapshot.data!.data() as Map<String, dynamic>;
-              String colorName = userData["fcolor"].toString().toLowerCase();
-              
-              Color selectedColor = ColorMapper.getColor(colorName);
-              
-              return Container(
-                color: selectedColor,
-                child: Center(
-                  child: Column(
-                    children: [
-                      Text("Welcome, "+userData["username"], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
-                        
-                      
-                      ElevatedButton(onPressed: () {
-                        setState((){
-                          _showImage = !_showImage;
-                        });
-                      }, child: Text('Celebrate'))
-                    ],
-                  ),
-                  
-                ),
-              
-                
-              );
-            } else if (snapshot.hasError) {
-                return Center(child: Text('Error${snapshot.error}')
-                );
-            }
-            
-              
-            
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-            
-          },
-        ),
-        
-
-        bottomNavigationBar: _showImage
-        ? Container(
-          color: Colors.grey[200],
-          height: 150,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-                Image.network(
-                  'https://media.tenor.com/RcX3hUY425kAAAAi/toothless-dragon-toothless.gif',
-                  height: 80,
-                  width: 80,
-                ),
-
-                SizedBox(height: 8),
-
-                Text("Happy Birthday! ${currentUser.email!.split('@')[0]}",
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Center(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                "What are you looking for?",
                 style: TextStyle(
-                  fontSize: 18,
                   fontWeight: FontWeight.bold,
-                ),)
-              
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(Icons.restaurant, size: 70,),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FoodPage()),
+                  );
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.house, size: 70),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HousingPage()),
+                  );
+                },
+              ),
             ],
           ),
-        )
-        : SizedBox(),
-        
-        
-        
-        // appBar: AppBar(
-        //   body: Co
-        //   title: Text('Add Sensor',
-        //       style: TextStyle(color: Colors.black), textAlign: TextAlign.center),
-        //   backgroundColor: Colors.transparent,
-        //   elevation: 0,
-        // ),
-        );
-        
+          Flexible(
+            child: FoodPageList(),     
+          ),
+        ],
+      ),
+    );
   }
 }
 
-class ColorMapper{
-  static final Map<String, Color> colorMap = {
-    'red' : Colors.red,
-    'orange' : Colors.orange,
-    'yellow' : Colors.yellow,
-    'green' :Colors.green,
-    'blue' : Colors.blue,
-  };
+class FoodPageList extends StatelessWidget {
+  final CollectionReference _kaon = FirebaseFirestore.instance.collection("kaon");
 
+  @override Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 400,
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _kaon.orderBy('averageRating', descending: true).snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshots) {
+                  if (snapshots.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(color: Colors.green),
+                    );
+                  }
+                  if (snapshots.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshots.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final DocumentSnapshot records = snapshots.data!.docs[index];
+                        final reviewsCollection = records.reference.collection('reviews');
 
+                        return StreamBuilder<double>(
+                          stream: getAverageRatingStream(reviewsCollection, records.reference),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return CircularProgressIndicator();
+                            }
+                            if (snapshot.hasData) {
+                              final averageRating = snapshot.data!;
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pushNamed(context, records["name"]);
+                                  },
+                                  child: Slidable(
+                                    startActionPane: ActionPane(motion: StretchMotion(), children: []),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors.grey,
+                                          width: 1.0,
+                                        ),
+                                        borderRadius: BorderRadius.circular(8.0),
+                                      ),
+                                      child: ListTile(
+                                        title: Text(records["name"]),
+                                        subtitle: Text(records["owner"] + '\n' + (records["fb link"]) + '\n' + (records["location"])),
+                                        trailing: Text('Average Rating: ${averageRating.toStringAsFixed(1)}'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }
+                            return Container(); // Placeholder while waiting for rating
+                          },
+                        );
+                      },
+                    );
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(color: Colors.red),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-static Color getColor(String colorName) {
-  return colorMap[colorName.toLowerCase()] ?? Colors.black;
-}
+  Stream<double> getAverageRatingStream(CollectionReference reviewsCollection, DocumentReference documentReference) {
+    return reviewsCollection.snapshots().map((snapshot) {
+      if (snapshot.docs.isEmpty) {
+        return 0.0;
+      }
+
+      double totalRating = 0;
+      for (var doc in snapshot.docs) {
+        final data = doc.data();
+        if (data != null && data is Map<String, dynamic> && data.containsKey('rating')) {
+          final rating = data['rating'];
+          if (rating is num) {
+            totalRating += rating.toDouble(); // Assuming rating is a numeric value
+          }
+        }
+      }
+
+      final averageRating = totalRating / snapshot.docs.length;
+      documentReference.update({'averageRating': averageRating}); // Update Firestore with the average rating
+      return averageRating;
+    });
+  }
 }
