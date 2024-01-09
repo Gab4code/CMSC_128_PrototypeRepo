e9eb2e871256b620d5938c5f7ba6e77f27f8e07c
Gaberzzz <ktgabay@up.edu.ph>
2024-01-09 14:38:08 +0800

    added onboarding1.png and changed onboarding1.dart

--- kantinir_mobile_app/lib/screens/onboarding/onboarding1.dart
+++ kantinir_mobile_app/lib/screens/onboarding/onboarding1.dart
@@ -2,38 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:kantinir_mobile_app/screens/onboarding/onboarding2.dart';
 
 class Onboarding1 extends StatelessWidget {
-  const Onboarding1({super.key});
+  const Onboarding1({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
         onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => Onboarding2(),
-          ));
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => Onboarding2(),
+            ),
+          );
         },
         child: Center(
-          child: RichText(
-            text: const TextSpan(
-              children: <TextSpan>[
-                TextSpan(
-                  text: 'KAN',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 64,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins'),
-                ),
-                TextSpan(
-                  text: 'TINIR',
-                  style: TextStyle(
-                      color: Color(0xFF59CCB5),
-                      fontSize: 64,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
+          child: Image.asset(
+            'images/onboarding1.png', // Replace 'your_image_path_here' with the actual path to your image asset
+            // Set the height as per your requirement
           ),
         ),
       ),
